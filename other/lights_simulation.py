57272bbcb82b978f88f6701664fdac27bd896d18
Kubol307 <kubawilk63@gmail.com>
2023-09-13 11:25:47 +0200

    Add delay to lights

--- other/lights_simulation.py
+++ other/lights_simulation.py
@@ -16,6 +16,18 @@ class Lights:
     def change(self):
         self.red = not self.red
 
+    def draw(self, screen):
+        pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(self.x - 5, self.y - 5, 30, 60))
+        pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(self.x + 8, self.y + 50, 5, 40))
+        if self.red:
+            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(self.x, self.y, 20, 20))
+            pygame.draw.rect(screen,  (50, 50, 50), pygame.Rect(self.x, self.y + 30, 20, 20))
+        else:
+            pygame.draw.rect(screen,  (50, 50, 50), pygame.Rect(self.x, self.y, 20, 20))
+            pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(self.x, self.y + 30, 20, 20))
+
+
+
 class Settings:
     def __init__(self):
         self.speed = 2
@@ -53,19 +65,19 @@ class Simulation(pygame.sprite.Sprite):
         self.v_cars = []
         # self.h_cars.append(self.car)
 
-        self.h_lights = Lights(1020, 900)
-        self.v_lights = Lights(900, 1020, True)
+        self.h_lights = Lights(1010, 850)
+        self.v_lights = Lights(850, 1010, True)
 
-        self.line = 100
-        self.change_penalty = 500
+        self.line = 50
+        self.distance = 250
+        self.change_penalty = 2000
 
         self.start_time = time.time()
-        self.v_waiting = False
-        self.h_waiting = False
-        self.change_time = 2
+        self.time_passed = 0
+        self.v_changing = False
+        self.h_changing = False
+        self.change_time = 1
 
-        self.h_start_time = time.time()
-        self.v_start_time = time.time()
         # self.font = pygame.font.SysFont(self.settings.font, self.settings.font_size)
         # self.font1 = pygame.font.SysFont(self.settings.font, 30)
 
@@ -83,7 +95,7 @@ class Simulation(pygame.sprite.Sprite):
             if h == 1:
                 self.h_cars.append(Car(self.settings, True))
 
-            v = np.random.binomial(1, 0.009)
+            v = np.random.binomial(1, 0.003)
             if v == 1:
                 self.v_cars.append(Car(self.settings, False))
             
@@ -112,11 +124,16 @@ class Simulation(pygame.sprite.Sprite):
 
             h_cost = 0
             for car in self.h_cars:
-                if car.x >= self.h_lights.x and car.x <= self.h_lights.x + self.line:
-                    car.cost += 15
-                    h_cost += car.cost 
-                else:
-                    car.cost = 0
+                if not self.v_changing and not self.h_changing:
+                    if car.x >= self.h_lights.x and car.x <= self.h_lights.x + self.distance:
+                        car.cost += 15
+                        h_cost += car.cost 
+                    elif car.x >= self.h_lights.x and car.x <= self.h_lights.x + self.line:
+                        car.cost += 50
+                        h_cost += car.cost 
+                    else:
+                        car.cost = 0
+
                 for another_car in self.h_cars:
                     if another_car.x <= car.x - 12 and another_car.x >= car.x - 15 and another_car.moving == False:
                         car.moving = False
@@ -128,11 +145,16 @@ class Simulation(pygame.sprite.Sprite):
 
             v_cost = 0
             for car in self.v_cars:
-                if car.y >= self.v_lights.y and car.y <= self.v_lights.y + self.line + 30:
-                    car.cost += 10
-                    v_cost += car.cost 
-                else:
-                    car.cost = 0
+                if not self.v_changing and not self.h_changing:
+                    if car.y >= self.v_lights.y and car.y <= self.v_lights.y + self.distance:
+                        car.cost += 15
+                        v_cost += car.cost 
+                    elif car.y >= self.v_lights.y and car.y <= self.v_lights.y + self.line:
+                        car.cost += 15
+                        v_cost += car.cost 
+                    else:
+                        car.cost = 0
+
                 for another_car in self.v_cars:
                     if another_car.y <= car.y - 12 and another_car.y >= car.y - 15 and another_car.moving == False:
                         car.moving = False
@@ -145,46 +167,39 @@ class Simulation(pygame.sprite.Sprite):
                 except:
                     pass
 
-            self.h_time_passed = time.time() - self.h_start_time
-            self.v_time_passed = time.time() - self.v_start_time
+            if self.v_changing or self.h_changing:
+                self.time_passed = time.time() - self.start_time
+                print(self.time_passed)
 
-            if h_cost > v_cost + self.change_penalty and self.h_lights.red and not (self.h_waiting or self.v_waiting):
-                self.v_lights.red = True
-                self.h_start_time = time.time()
-                self.h_waiting = True
-            elif v_cost > h_cost + self.change_penalty and self.v_lights.red and not (self.h_waiting or self.v_waiting):
-                self.h_lights.red = True
-                self.v_start_time = time.time()
-                self.v_waiting = True
-            if self.v_waiting and self.v_time_passed > self.change_time:
-                self.v_lights.red = False
-                self.v_waiting = False
-            if self.h_waiting and self.h_time_passed > self.change_time:
-                self.h_lights.red = False
-                self.h_waiting = False
-            print(f'h cost: {h_cost}')
-            print(f'v cost: {v_cost}')
 
-            
-            pygame.draw.rect(self.screen, (200, 200, 200), pygame.Rect(self.v_lights.x - 5, self.v_lights.y - 5, 30, 60))
-            pygame.draw.rect(self.screen, (200, 200, 200), pygame.Rect(self.h_lights.x - 5, self.h_lights.y - 5, 30, 60))
+            if not self.v_changing and not self.h_changing:
+                if h_cost > v_cost + self.change_penalty and self.h_lights.red:
+                    self.v_lights.change()
+                    self.v_lights.draw(self.screen)
+                    self.start_time = time.time()
+                    self.h_changing = True
+                    print('both red')
 
-            if self.h_lights.red:
-                pygame.draw.rect(self.screen, (255, 0, 0), pygame.Rect(self.h_lights.x, self.h_lights.y, 20, 20))
-                pygame.draw.rect(self.screen,  (50, 50, 50), pygame.Rect(self.h_lights.x, self.h_lights.y + 30, 20, 20))
-            else:
-                pygame.draw.rect(self.screen,  (50, 50, 50), pygame.Rect(self.h_lights.x, self.h_lights.y, 20, 20))
-                pygame.draw.rect(self.screen, (0, 255, 0), pygame.Rect(self.h_lights.x, self.h_lights.y + 30, 20, 20))
+                elif v_cost > h_cost + self.change_penalty and self.v_lights.red:
+                    self.h_lights.change()
+                    self.h_lights.draw(self.screen)
+                    self.start_time = time.time()
+                    self.v_changing = True
+                    print('both red')
 
-            if self.v_lights.red:
-                pygame.draw.rect(self.screen, (255, 0, 0), pygame.Rect(self.v_lights.x, self.v_lights.y, 20, 20))
-                pygame.draw.rect(self.screen, (50, 50, 50), pygame.Rect(self.v_lights.x, self.v_lights.y + 30, 20, 20))
-            else:
-                pygame.draw.rect(self.screen, (50, 50, 50), pygame.Rect(self.v_lights.x, self.v_lights.y, 20, 20))
-                pygame.draw.rect(self.screen, (0, 255, 0), pygame.Rect(self.v_lights.x, self.v_lights.y + 30, 20, 20))
-            
+            if self.v_changing and self.time_passed > self.change_time:
+                self.v_lights.change()
+                self.v_changing = False
+                self.time_passed = 0
+                print('v green')
+            if self.h_changing and self.time_passed > self.change_time:
+                self.h_lights.change()
+                print('h green')
+                self.time_passed = 0
+                self.h_changing = False
 
-            
+            self.v_lights.draw(self.screen)
+            self.h_lights.draw(self.screen)
 
             pygame.display.flip()
             self.clock.tick(60)
